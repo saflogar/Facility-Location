0981a45b0d51d4683748ba785cf9c2c2a05369ec
Sergio <safgflores@hotmail.com>
2014-05-30 22:52:50 -0500

    Agregado clase Datos para abrir archivos

--- src/Archivos/Datos.java
+++ src/Archivos/Datos.java
@@ -6,10 +6,56 @@
 
 package Archivos;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author sergio
  */
 public class Datos {
+    private Scanner archivo;
+    private Scanner line;
+    // Numero de facilidades y Numero de clientes
+    private int NF , NC;
+    //Matriz de NF X NC que almacena el costo de la conexion entre facilidad y cliente
+    private int [][] c;
+    private int[] f;
+
+    
+    public Datos(File fName) throws FileNotFoundException
+    {
+            archivo = new Scanner(new FileReader(fName));
+            line = new Scanner (archivo.nextLine());
+            NF = line.nextInt();
+            NC = line.nextInt();
+            line = new Scanner (archivo.nextLine());
+            f = new int[NF];
+            c = new int [NC][NF];
+            
+            for (int i = 0; i < NF ; i++)
+            {
+               f[i] = line.nextInt();
+            }
+            
+
+            for (int j = 0; j < NC; j++)
+            {
+                line = new Scanner (archivo.nextLine());
+                for (int i =0; j < NF; i++)
+                {
+                    c[j][i] = line.nextInt();
+                }
+              
+            }
+    
+    }
+    
+    
+    
     
 }
